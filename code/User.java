780bbc07064485b4b1b8e6bbda5eb649b07b3ac5
Ivan <isobonja@gmail.com>
2019-10-26 15:31:31 -0400

    Update User.java

--- code/User.java
+++ code/User.java
@@ -2,15 +2,8 @@
 
 public class User extends Player{
    
-   private boolean isUserTurn;
-   
    public User(){
       super();
-      isUserTurn = true
-   }
-   
-   public void goFish(){
-      
    }
    
    public void getCard(){
