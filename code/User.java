4a19e329f9aebfbefafd55e39fce87a38c33a535
Ivan <isobonja@gmail.com>
2019-10-26 14:44:22 -0400

    Create User.java

---
+++ code/User.java
@@ -0,0 +1,20 @@
+// User class
+
+public class User extends Player{
+   
+   private boolean isUserTurn;
+   
+   public User(){
+      super();
+      isUserTurn = true
+   }
+   
+   public void goFish(){
+      
+   }
+   
+   public void getCard(){
+   
+   }
+
+}
