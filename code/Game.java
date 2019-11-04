7bb19aa64c52f167812f724b9453b5f9a2d3b4d7
aamin20 <54999523+aamin20@users.noreply.github.com>
2019-11-03 19:27:03 -0500

    Add files via upload
    Added check from 101 to 104 to make sure user asks for only cards they have

--- code/Game.java
+++ code/Game.java
@@ -38,8 +38,8 @@ public class Game{
 	 * The computer
 	 */
 	private Computer cp;
-   
-   private Scanner in;
+
+	private Scanner in;
 
 
 	public Game(int theme){
@@ -47,7 +47,7 @@ public class Game{
 		this.theme = theme;
 		this.deck = new ArrayList<>();
 		this.isUserTurn = true;
-      this.in = new Scanner(System.in);
+		this.in = new Scanner(System.in);
 
 		//creates deck of 52 cards
 		for (int i=1; i<=52; i++){
@@ -74,45 +74,58 @@ public class Game{
 	 * the game is over
 	 */ 
 	private void gameLoop(){
-      cp.checkPairs();
-      user.checkPairs();
-      
+		cp.checkPairs();
+		user.checkPairs();
+
 		while (user.getSizeHand() != 0 && cp.getSizeHand()!= 0 && deck.size() != 0) 
 		{
-         System.out.println("CP SCORE: " + cp.getScore() + "\tUSER SCORE: " + user.getScore() + "\tCARDS IN DECK: " + deck.size());
-         //System.out.println("test");
-         System.out.println(cp.getHand());
-         //System.out.println(cp.getSizeHand());
-         System.out.println(user.getHand());
-         //System.out.println(user.getSizeHand());
+			System.out.println("CP SCORE: " + cp.getScore() + "\tUSER SCORE: " + user.getScore() + "\tCARDS IN DECK: " + deck.size());
+			//System.out.println("test");
+			System.out.println("CP HAND: " + cp.getHand());
+			//System.out.println(cp.getSizeHand());
+			System.out.println("USER HAND: " + user.getHand());
+			//System.out.println(user.getSizeHand());
 			//game loop
 			if(isUserTurn){
-            System.out.println("User's turn:\n");
+				System.out.println("User's turn:\n");
 				//user turn
-            System.out.print("Enter the value you would like to ask for: ");
-            int value = in.nextInt();
-            
-            //*** Limit player's value choice to between 1 and 13 ***
-            
+				System.out.print("Enter the value you would like to ask for: ");
+				int value = in.nextInt();
+				
+				ArrayList<Integer> currentUserCards = new ArrayList<>();
+				for(int i = 0; i < user.getHand().size(); i++) {
+					currentUserCards.add(user.getHand().get(i).getValue());
+				}
+				
+				//*** make sure player is asking for a card that they have
+				while (currentUserCards.contains(value) == false) {
+					System.out.println("\nPlese ask for card you already havve in your own deck!");
+					System.out.print("Enter the value you would like to ask for: ");
+					value = in.nextInt();
+				}
+				
+				//*** Limit player's value choice to between 1 and 13 *** 
+				//Ahmad note: not sure we need this check once we check that the user is only asking for cards they have
+
 				user.getCard(this, cp, value);
 				user.checkPairs();
 			}else{
 				//comp turn
-            System.out.println("Computer's turn:\n");
+				System.out.println("Computer's turn:\n");
 				cp.getCard(this, user);
 				cp.checkPairs();
 			}
 		}
-      
-      System.out.println("User score: " + user.getScore() + "     Computer score: " + cp.getScore());
-      
+
+		System.out.println("User score: " + user.getScore() + "     Computer score: " + cp.getScore());
+
 		if(user.getScore()>cp.getScore()) {
 			System.out.println("You win!");
 		}else if(cp.getScore()>user.getScore()){
 			System.out.println("Computer wins!");
 		}else{
-         System.out.println("Tie!");
-      }
+			System.out.println("Tie!");
+		}
 		//ending
 	}
 
@@ -124,13 +137,12 @@ public class Game{
 	public ArrayList<Card> getDeck(){
 		return this.deck;
 	}
-   
-   public void setTurn(boolean b){
-      this.isUserTurn = b;
-   }
-   
-   public static void main(String[] args){
-      new Game(0);
-   }
-}
 
+	public void setTurn(boolean b){
+		this.isUserTurn = b;
+	}
+
+	public static void main(String[] args){
+		new Game(0);
+	}
+}
