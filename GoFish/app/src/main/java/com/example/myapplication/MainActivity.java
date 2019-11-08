c96e2f324cd0265fb8f94f32590a1ed1f7dcc76e
aamin20 <54999523+aamin20@users.noreply.github.com>
2019-11-08 16:49:40 -0500

    Full Screen Update

--- GoFish/app/src/main/java/com/example/myapplication/MainActivity.java
+++ GoFish/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
     }
 
     public void getSpeechInput(View view){
