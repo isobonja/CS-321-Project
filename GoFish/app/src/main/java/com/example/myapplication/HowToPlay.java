7e8b5d311ddeb0c954f0b30340b8a0cd47cbd8b2
esgulhan <shimalg98@gmail.com>
2019-11-09 18:12:35 -0500

    Updated java files

--- GoFish/app/src/main/java/com/example/myapplication/HowToPlay.java
+++ GoFish/app/src/main/java/com/example/myapplication/HowToPlay.java
@@ -10,12 +10,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class StartPageController extends AppCompatActivity{
+public class HowToPlay extends AppCompatActivity{
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_page);
+        setContentView(R.layout.how_to_play);
 
         int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -39,40 +38,17 @@ public class StartPageController extends AppCompatActivity{
         getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
     }
 
-
-    public void getSpeechInputTwo(View view){
+    public void getSpeechInputGoBack(View view){
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 11);
+            startActivityForResult(intent, 12);
         } else {
             Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
 
-    /*
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case 11:
-                if (resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String userInput = result.get(0);
-                    userInput = userInput.toLowerCase();
-                    if (userInput.contains("back") ){
-                        this.finish();
-                    }
-
-                }
-                break;
-        }
-    }
-
-     */
 }
--- GoFish/app/src/main/java/com/example/myapplication/MainActivity.java
+++ GoFish/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +24,27 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+    }
+
+    public void full_screen(View view){
+        int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
         getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
     }
 
     public void clickButton(View view){
-        Toast.makeText(this, "Please only use the Microphone's voice recognition capabiltiy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Please only use the Microphone's voice recognition capability", Toast.LENGTH_SHORT).show();
     }
-    
+
     public void getSpeechInput(View view){
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -41,7 +54,33 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, 10);
         } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getSpeechInputTwo(View view){
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 11);
+        } else {
+            Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getSpeechInputGoBack(View view){
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 12);
+        } else {
+            Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -59,13 +98,42 @@ public class MainActivity extends AppCompatActivity {
                         setContentView(R.layout.start_page);
                     }
                     else if (userInput.contains("how to play") && !(userInput.contains("start"))){
-                        Toast.makeText(this, "HOW TO PLAY ACTIVITY ACTIVITY NOT YET MADE", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.how_to_play);
                     }
                     else{
-                        Toast.makeText(this, "Please either say 'Start' or 'How To Play'", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Please either say 'Start' or 'How To Play'", Toast.LENGTH_LONG).show();
                     }
                 }
+
+            case 11:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String userInput = result.get(0);
+                    userInput = userInput.toLowerCase();
+                    if (userInput.contains("back") ){
+                        setContentView(R.layout.activity_main);
+                    }
+                    else{
+                        Toast.makeText(this, "Say 'Back' to  go back to the main menu", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+            case 12:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String userInput = result.get(0);
+                    userInput = userInput.toLowerCase();
+                    if (userInput.contains("back") ){
+                        setContentView(R.layout.activity_main);
+                    }
+                    else{
+                        Toast.makeText(this, "Say 'Back' to  go back to the main menu", Toast.LENGTH_LONG).show();
+                    }
+
+                }
                 break;
+
         }
     }
 }
