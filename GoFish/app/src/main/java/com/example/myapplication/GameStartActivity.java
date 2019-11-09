ac88cdb81f34aaa61eb6c024697af382f0c8e06b
knguye91 <kevin711nguyen@yahoo.com>
2019-11-09 18:01:12 -0500

    Create GameStartActivity.java

---
+++ GoFish/app/src/main/java/com/example/myapplication/GameStartActivity.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameStartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+    }
+
+}
